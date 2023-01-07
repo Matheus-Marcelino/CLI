b92784ab30e7d419a27692f357adbd6e9d60dc15
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2023-01-07 15:35:30 -0300

    craindo o save path

--- data_user/crypt/__init__.py
+++ data_user/crypt/__init__.py
@@ -16,6 +16,19 @@ class HashGenerator:
         text: str = md5(encoded).hexdigest()
         return text
 
+    def one_time_encryption(self, text: str, type: str) -> str:
+        def save_path() -> None:
+            """Cria o arquivo com a criptografia própria"""
+            def auxiliar() -> None:  # escreve o arquvio na primeira inicialização
+                with open(f'{path}\coden\key.txt', 'w+', encoding='utf-8') as file:
+                    for digito in digits:
+                        file.write(f'{digito}={self.generator_token(complexidade)}\n')
+                    for letra in LETTERS:
+                        file.write(f'{letra}={self.generator_token(complexidade)}\n')
+                    for symbol in punctuation:
+                        file.write(f'{symbol}={self.generator_token(complexidade)}\n')
+
+
 
 if __name__ == "__main__":
     hash = HashGenerator()
