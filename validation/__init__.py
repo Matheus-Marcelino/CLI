f7f70e021b73c3100ce6c9332a50776ae6b2623f
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2023-01-09 19:08:21 -0300

    calculador de tempo

---
+++ validation/__init__.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+def Calculate_time(func):
+    def wrapper(*args, **kwgs):
+        time_inicial = datetime.now()
+
+        resultado = func(*args, **kwgs)
+
+        time_final = datetime.now()
+
+        time_total = time_final - time_inicial
+        return resultado, time_total
+    return wrapper
+
+# str(function()[1])[:7]
