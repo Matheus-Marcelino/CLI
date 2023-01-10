15160f28747d564d778901030a832ba51feef155
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2023-01-10 13:21:54 -0300

    criando os init das classes e validação/ acesso do mp3 feito

--- main.py
+++ main.py
@@ -22,6 +22,39 @@ class Main:
         self.__opc_valid: tuple[str] = ('Y', 'S', 'YES', 'SIM')
         self.__opc_invalid: tuple[str] =  ('NÃO', 'NAO',  'NO', 'NOT')
 
+    def init_music_converter(self):
+        while True:
+            self.__window.title('Mp3 Converter', 11,35, 25)
+            print('\n\033[1;33mCertifique-se que o arquivo esteja na pasta "input"')
+            opcion = str(input('Digite o nome do arquivo: '))
+            opcion_verify = str(input(f'O nome "{opcion}" está correto?[Y/N]: ')).upper().strip()
+
+            if opcion_verify in self.__opc_invalid:
+                while True:
+                    opcion = str(input('\nDigite o nome do arquivo: '))
+                    opcion_verify = str(input(f'O nome "{opcion}" está correto?[Y/N]: ')).upper().strip()
+                    if opcion_verify in self.__opc_valid:
+                        break
+
+            opcion_retry = str(input('\nDigite novamente o nome do arquivo: '))
+
+            while opcion_retry != opcion:
+                print('\nOs nomes não batem digite corretamente!')
+                opcion_retry = str(input('Digite o nome correto do arquivo: '))
+
+            print()
+            self.__mp3 = ConverterMp3(opcion_retry)
+            self.__mp3.convert_mp3(opcion_retry)
+            sleep(3)
+            break
+
+    def init_yt(self):
+        self.__yt = YouDownTube('aaa')
+
+    def init_image_converter(self):
+        self.__imgc = ImageConverter('aaaa')
+
+
     def __home(self):
         self.__window.title(Fore.BLUE + '\033[1mMenu de opções', 11,35, 25)
         self.__window.table(21)
