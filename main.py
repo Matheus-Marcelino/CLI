824abf3afe209efc6e6570246dff5138b4d19989
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2023-01-09 23:07:55 -0300

    Importando bibliotecas necessárias

---
+++ main.py
@@ -0,0 +1,9 @@
+from colorama import init
+from window import Window
+from error_treatment import tratamento
+from validation.crypt import HashGenerator
+from validation.json_manager import JsonManager
+from funcionalidades.yt_downloader import YouDownTube
+from funcionalidades.mp3_converter import ConverterMp3
+from funcionalidades.image_converter import ImageConverter
+init(True)
