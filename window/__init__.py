acfe91fa2823fd022c77a4918a3d8f878e37d2d0
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2023-01-11 02:11:03 -0300

    window finalizado

--- window/__init__.py
+++ window/__init__.py
@@ -1,15 +1,14 @@
 from os import system
-import colorama
-colorama.init()
+from colorama import Fore
 
 class Window:
     def clear_terminal(self) -> None:
         system('cls')
 
     def title(self, text: str, lenght: int, space: int, dist: int) -> None:
-        print(' ' * dist, colorama.Fore.MAGENTA + '=-=' * lenght)
+        print(' ' * dist, Fore.MAGENTA + '=-=' * lenght)
         print(('' if space == 0 else ' ' * space) , f'{text}')
-        print(' ' * dist, colorama.Fore.MAGENTA + '=-=' * lenght)
+        print(' ' * dist, Fore.MAGENTA + '=-=' * lenght)
         
     def table(self, dist: int) -> None:
         print(' ' * (dist+1), '\033[1m1- Music Converter | 3- Youtube Downloader\n',
@@ -17,8 +16,8 @@ class Window:
               ' ' * int(dist + (dist/1.4)), '5- Exit')
         
     def table_yt(self) -> None:
-        print('Escolha a resoulção que você deseja, '
+        print('\033[1;31mEscolha a resoulção que você deseja, '
               'digite os numeros ou a própria resolução\n'
               '1: Baixa resolução |  720p, 480p,\n'
               '2: Média resolução |  360p, 240p,\n'
-              '3: Alta resolução  |     144p\n')
+              '3: Alta resolução  |     144p\033[m\n')
