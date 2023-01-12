055171202be0dc3ece42179891e82b69613bdd7c
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2023-01-12 01:30:22 -0300

    Image reconfigurado

--- funcionalidades/image_converter/__init__.py
+++ funcionalidades/image_converter/__init__.py
@@ -1,33 +1,26 @@
-from pathlib import Path
-from sys import path
-caminho = Path(__file__)
-caminho = caminho.parent.parent.parent / 'error_treatment'
-caminho = str(caminho).replace('\\', '/')
-path.append(caminho)
-from tratamento import Treatment
-
 from PIL.Image import open as Open
 from os import listdir, rename, mkdir, remove
+from os.path import dirname, realpath
 
 
 class ImageConverter:
     def __init__(self, name_file_image: str) -> None:        
-        global caminho
         self.__file: str = name_file_image
         self.__extension: list = ['jpg', 'jpeg', 'png']
-        self.__caminho = caminho[:-15] + 'input/' + name_file_image
-        self.__trat = Treatment()
+        self.__caminho = str(dirname(realpath(__file__)))[:-31] + 'input/' + name_file_image
+        self.__caminho = self.__caminho.replace('/', '\\')
 
-    def search_file(self) -> (str | None):
+    def __search_file(self) -> (bool | str):
         search: list = listdir('input')
         if self.__file in search:
             self.__file: list = self.__file.split('.')
             self.__extension.remove(f"{self.__file[-1]}")
+            return True
         else:
             return ('\033[1;31mSua imagem não foi encontrada, ' 
                     'verifique se realmente está na pasta "input"\033[m')
 
-    def converter(self) -> bool:
+    def converter(self) -> (bool | str):
         def move_image() -> None:
             try:
                 rename(self.__file, f'output/{self.__file}')
@@ -41,25 +34,30 @@ class ImageConverter:
                     remove(self.__file)
                     print('\n\033[1;31mImagem já existente!\033[m')
 
-        @self.__trat.error_treatment(type_of_error=(FileNotFoundError),
-                                   message=(True, '\n\033[1;31mO arquivo não se encontra mais na pasta "input"\033[m'))
         def convert() -> (bool | None):
-            self.__file: str = f'{self.__file[0]}.{opc}'
-            image = Open(self.__caminho)
-            image_convertida = image.convert('RGB')
-            image_convertida.save(self.__file)
-            move_image()
-            return True
+            try:
+                self.__file: str = f'{self.__file[0]}.{opc}'
+                image = Open(self.__caminho)
+                image_convertida = image.convert('RGB')
+                image_convertida.save(self.__file)
+                move_image()
+                return True
+            except FileNotFoundError:
+                print('\n\033[1;31mO arquivo não se encontra mais na pasta "input"\033[m')
 
-        xts: str = ' | '.join(arg for arg in self.__extension)
-        while True:
-            opc: str = str(input('\nQual tipo de extensão você quer para sua imagem?\n'
-                                 f'{xts}\n>> '))
-            if opc in xts:
-                break
-            else:
-                print('\n\033[1;31mOpção inexistente, escolha uma valida!\033[m')
+        search: bool | str = self.__search_file()
+        if search is True:
+            xts: str = ' | '.join(arg for arg in self.__extension)
+            while True:
+                opc: str = str(input('\nQual tipo de extensão você quer para sua imagem?\n'
+                                    f'{xts}\n>> '))
+                if opc in xts:
+                    break
+                else:
+                    print('\n\033[1;31mOpção inexistente, escolha uma valida!\033[m')
 
-        data = convert()
-        if data is True:
-            return True
+            data = convert()
+            if data is True:
+                return True
+        else:
+            return search
