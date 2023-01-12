e02d0043cc82e4511331450d60fa2913d96aba25
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2023-01-12 02:14:42 -0300

    mp3 debugado

--- funcionalidades/mp3_converter/__init__.py
+++ funcionalidades/mp3_converter/__init__.py
@@ -3,24 +3,16 @@ Converta seu mp4 para mp3
 """
 from os import listdir, mkdir, remove, rename
 from moviepy.editor import VideoFileClip
-from pathlib import Path
-from sys import path
-caminho = Path(__file__)
-caminho = caminho.parent.parent.parent / 'error_treatment'
-caminho = str(caminho).replace('\\', '/')
-path.append(caminho)
-from tratamento import Treatment
 
 
 class ConverterMp3:
     """Conversor de extensões"""
     def __init__(self, name_mp4: str) -> None:
         self.__file = name_mp4
-        self.__trat = Treatment()
 
-    def __checking(self):
+    def __checking(self) -> (bool | None):
         try:
-            if self.__file.split('.')[1].replace(' ', '') != ('mp4', 'mkv'):
+            if self.__file.split('.')[1].replace(' ', '') != 'mp4':
                 return True
         except IndexError:
             self.__file = self.__file + '.mp4'  # Extensão padrão
@@ -31,17 +23,19 @@ class ConverterMp3:
         if self.__checking():
             return '\033[1;31mExtensão não suportada!\033[m' 
 
-        @self.__trat.error_treatment((OSError, FileNotFoundError),
-                                   (True, '\033[1;31mArquivo não encontrado ou pasta não encontrada! '
-                                    'Verifique se o arquivo\nestá na pasta "input" ou se a pasta "input" existe.\033[m'))
         def converter() -> bool:
             """Função principal para converter"""
-            video_clip = VideoFileClip(filename=f'input/{self.__file}')
-            audio_clip = video_clip.audio
-            audio_clip.write_audiofile(self.__file)
-            audio_clip.close()
-            video_clip.close()
-            return True
+            try:
+                video_clip = VideoFileClip(filename=f'input\{self.__file}')
+                self.__file = self.__file[:-1] + '3'
+                audio_clip = video_clip.audio
+                audio_clip.write_audiofile(self.__file)
+                audio_clip.close()
+                video_clip.close()
+                return True
+            except (OSError, FileNotFoundError):
+                print('\033[1;31mArquivo não encontrado ou pasta não encontrada! '
+                      'Verifique se o arquivo\nestá na pasta "input" ou se a pasta "input" existe.\033[m')
 
         def move_file() -> None:
             """Função que joga para a pasta output"""
@@ -49,8 +43,11 @@ class ConverterMp3:
                 rename(self.__file, f'output/{self.__file}')
                 print('\033[1;32mMp3 Concluído, Ela se encontra na pasta output!\033[m')
             except FileNotFoundError:
-                mkdir('output')
-                move_file()
+                try:
+                    mkdir('output')
+                    move_file()
+                except FileExistsError:
+                    pass
             except FileExistsError:
                 search: list = listdir('output')
                 if self.__file in search:
@@ -61,3 +58,6 @@ class ConverterMp3:
             move_file()
         else:
             return False
+
+a = ConverterMp3('come a little closer - cage the elephant (tradução).mp4')
+a.convert_mp3()
